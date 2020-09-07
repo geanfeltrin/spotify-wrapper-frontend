392fd1f0ff318b1974bdade283c99f2023d2df9c
Gean Feltrin <gean_costa@hotmail.com>
2020-09-07 16:20:00 -0300

    Create instace of spotify wrapper

---
+++ src/spotify.js
@@ -0,0 +1,9 @@
+import SpotifyWrapper from 'spotify-wrapper';
+
+
+const spotify = new SpotifyWrapper({
+  token: 'BQCV64MiVF7_1XGrGe03oGH0ibMbHDC_5Ja0X1Re5eJ-fFZifvYaMAH787vuJQDLlyD8iHJFDv8FqVKTzY6JLvwlTSxwD7FxOveA7echquFEVNxj4djeEg-MpUAbu1SuLCaO7aC2KZltSPVVBFK59GFieAwxdaU',
+});
+
+
+export default spotify;
